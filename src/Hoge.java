761c2ba6598904f43d113e5ccb48d4f88c07c36e
chiyonofujio <chiyonofujio@gmail.com>
2014-04-15 13:05:42 +0900

    gegegebo

--- src/Hoge.java
+++ src/Hoge.java
@@ -6,7 +6,7 @@ public class Hoge {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("test");
+		System.out.println("test2");
 		System.out.println("test");
 
 	}
